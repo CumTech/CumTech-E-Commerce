513ed2ca4e88f02c4d0b9e87739e001a468880ce
Alan Jair Almaguer <47544063+alanjair226@users.noreply.github.com>
2023-03-14 15:29:55 -0600

    panatalla login solo falta el fondo

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.udavdcuaydayvdauyasvhkdsvc to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text>hola</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  topContainer: {
+    flex: 2,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+
+  },
+  centerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  bottomContainer:{
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    fontFamily: 'coco-goose',
+    fontSize: 16,
+    fontWeight: 300,
+    color: 'black',
+    alignItems: 'center',
+  },
+  input: {
+    height: 35,
+    width: 232,
+    marginBottom: 10,
+    borderWidth: 10,
+    borderColor: 'transparent', // borde transparente para que la sombra se muestre en la parte interior
+    backgroundColor: '#fff', // color de fondo para que la sombra sea visible
+    overflow: 'hidden', // ocultar la parte exterior de la sombra
+    padding: 10,
+    borderRadius: 24,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 1, // establece el valor de shadowRadius en 1
+    elevation: 1.5, // establece el valor de elevation en 1
+  }
 });
