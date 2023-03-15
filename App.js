44c3060c82b55d33625a4ddd64a5255fb762320b
LurchingDart <luizalferez25@gmail.com>
2023-03-14 19:14:11 -0600

    Limpieza del archivo App.js

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import NavigationStack from './src/navigation/NavigationStack';
 import NavigationTab from './src/navigation/NavigationTab';
-import React, { useState, useEffect } from 'react';
 
 export default function App() {
 
@@ -16,11 +12,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
