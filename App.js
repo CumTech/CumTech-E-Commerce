0995f73073c443917f006cb31661e6a4f8acf63f
Alan Almaguer <alalmag2@gmail.com>
2023-03-16 19:46:12 -0600

    Login and Register Screen finished

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationTab from './src/navigation/NavigationTab';
-
+import NavigationStack from './src/navigation/navigationStack';
 export default function App() {
 
   return (
     <NavigationContainer>
-      <NavigationTab />
+      <NavigationStack />
     </NavigationContainer>
   );
 }
