e63bf7c5244c489ad0c135e57b49965e982aa230
LurchingDart <luizalferez25@gmail.com>
2023-03-20 14:14:42 -0600

    Correccion mini

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
 import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
-import { Text} from 'react-native';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationTab from './src/navigation/NavigationTab';
 import * as Font from 'expo-font';
 
-// async function loadFonts() {
-//   await Font.loadAsync({
-//     'coco-goose': require('./assets/fonts/CocogooseProLight-trial.ttf'),
-//     'inter': require('./assets/fonts/Inter-Regular.ttf'),
-//     'monda': require('./assets/fonts/Monda-Regular.ttf'),
-//   });
-// }
+async function loadFonts() {
+  await Font.loadAsync({
+    'coco-goose': require('./assets/fonts/CocogooseProLight-trial.ttf'),
+    'inter': require('./assets/fonts/Inter-Regular.ttf'),
+    'monda': require('./assets/fonts/Monda-Regular.ttf'),
+  });
+}
 
 export default function App() {
 
-  // useEffect(() => {
-  //   loadFonts();
-  // }, []);
-
-  const [fontLoaded, setFontLoaded] = useState(false);
-
   useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        'coco-goose': require('./assets/fonts/CocogooseProLight-trial.ttf'), // Font can not have spaces
-      }); 
-      setFontLoaded(true);
-    } 
     loadFonts();
   }, []);
-
-  if (!fontLoaded) {
-    return <Text>Loading...</Text>;
-  }
 
   return (
     <NavigationContainer>
